a21fdf06651a2b77d3ef2dcbaed0891efafcdfbf
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-04-07 14:19:54 +0200

    Initial commit

---
+++ src/main/java/giuseppetavella/D1_menu_pizzeria/D1MenuPizzeriaApplication.java
@@ -0,0 +1,13 @@
+package giuseppetavella.D1_menu_pizzeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D1MenuPizzeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D1MenuPizzeriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/giuseppetavella/D1_menu_pizzeria/D1MenuPizzeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package giuseppetavella.D1_menu_pizzeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D1MenuPizzeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
