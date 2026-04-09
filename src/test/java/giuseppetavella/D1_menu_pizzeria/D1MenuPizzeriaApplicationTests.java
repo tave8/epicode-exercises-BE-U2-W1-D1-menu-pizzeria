493b88f0e3912a2571cb98e05f8a52014dee3a8e
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-04-09 15:36:56 +0200

    add tests: menu ha nome corretto, menu ha almeno 1 elemento

--- src/test/java/giuseppetavella/D1_menu_pizzeria/D1MenuPizzeriaApplicationTests.java
+++ src/test/java/giuseppetavella/D1_menu_pizzeria/D1MenuPizzeriaApplicationTests.java
@@ -1,13 +1,30 @@
 package giuseppetavella.D1_menu_pizzeria;
 
+import giuseppetavella.D1_menu_pizzeria.entities.Menu;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @SpringBootTest
 class D1MenuPizzeriaApplicationTests {
-
+	
+	@Autowired
+	private Menu menu;
+	
+	
 	@Test
-	void contextLoads() {
+	public void menuHaNomeCorretto() {
+		assertEquals("menu standard", menu.getNome().toLowerCase());
 	}
+	
+	@Test
+	public void menuHaAlmeno1Elemento() {
+		assertNotEquals(0, menu.getElementiMenu().size());
+	}
+	
+	
 
 }
